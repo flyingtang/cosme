88238bf49c074386a8b4d2982c9e0ee922866fd0
xiaogang <123456>
2018-09-09 00:52:30 +0800

    update go vendor and add install.sh

---
+++ bin/install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# 由于国内的原因，导致golang.org 的文件下载不下来，所有用这个脚本替代
+
+# 第一部分 安装git
+apk add git 
+
+# 第二部分
+rootdir=`pwd`
+echo "this pwd is $rootdir"
+
+golangpath="$rootdir/vendor/golang.org/x"
+echo "golangpath :"$golangpath
+
+if [ ! -d $golangpath ]; then
+    
+    mkdir -p  $golangpath
+    cd $golangpath
+
+    git clone https://github.com/golang/crypto.git
+    git clone https://github.com/golang/sys.git
+
+fi
+
+echo "start install govendor..."
+go get -u github.com/kardianos/govendor
+echo "end install govendor..."
+
+echo "start govendor sync -v..."
+govendor sync -v
+echo "completed !!!"
